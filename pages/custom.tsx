55a45160f1b2a2568fe324354c256819efcb7c96
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2021-10-15 00:59:25 +0700

    Add Custom page

---
+++ pages/custom.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+
+const CustomPage = () => (
+  <Layout title="Custom">
+    <h1>Custom</h1>
+  </Layout>
+);
+
+export default CustomPage;
