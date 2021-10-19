798372d84f8110b526209011a1a91527cc228b8f
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2021-10-19 23:43:05 +0700

    set Category, set Product, select Product

--- components/CustomizeLab.tsx
+++ components/CustomizeLab.tsx
@@ -1,21 +1,55 @@
 import Image from "next/image";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Pagination } from "swiper";
 import { format } from "@/lib/currency";
-import { getAllCategories, getGene } from "@/lib/db";
+import { getCategories } from "@/lib/db";
+import { Category } from ".prisma/client";
+import { Awaited } from "@/lib/types/common";
+import { CategoryType, GeneType, ProductType } from "@/lib/types/custom";
 
 SwiperCore.use([Pagination]);
+
 const MAX_COUNT = 6;
-type Awaited<T> = T extends PromiseLike<infer U> ? Awaited<U> : T;
 interface CustomizeLabProps {
-  categories: Awaited<ReturnType<typeof getAllCategories>>;
-  gene: Awaited<ReturnType<typeof getGene>>;
+  categories: CategoryType[];
+  gene: GeneType;
 }
-type ArrayElement<ArrayType extends readonly unknown[]> =
-  ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
 
 export default function CustomizeLab({ categories, gene }: CustomizeLabProps) {
+  const [selectedProducts, setSelectedProducts] = useState<ProductType[]>([]);
+  const [currentProduct, setCurrentProduct] = useState<ProductType | null>(null);
+  const [currentCategory, setCurrentCategory] = useState<CategoryType | null>(null);
+  const onCategoryClick = useCallback(
+    (id: number) => {
+      setCurrentCategory(categories.find((c) => c.id === id));
+    },
+    [categories]
+  );
+  const onBackButtonClick = () => {
+    setCurrentProduct(null);
+    if (currentCategory.parentId) {
+      setCurrentCategory(categories.find((c) => c.id === currentCategory.parentId));
+    } else {
+      setCurrentCategory(null);
+    }
+  };
+  const onProductClick = useCallback(
+    (id: number) => {
+      setCurrentProduct(
+        id === 0 ? null : currentCategory.products.find((product) => product.id === id)
+      );
+    },
+    [currentCategory]
+  );
+  const onSelectProduct = () => {
+    setCurrentProduct(null);
+    setSelectedProducts([
+      ...selectedProducts.filter((product) => product.id !== currentProduct.id),
+      currentProduct,
+    ]);
+  };
+
   return (
     <div className="relative h-full px-4 overflow-hidden">
       <div className="flex w-full h-[3.25rem] items-center justify-between mb-5">
@@ -51,57 +85,148 @@ export default function CustomizeLab({ categories, gene }: CustomizeLabProps) {
           height={545}
           alt={gene.description}
         />
+        {currentProduct && (
+          <div className="absolute" style={{ zIndex: currentProduct?.category?.layer }}>
+            <Image
+              src={currentProduct.variants[0].image}
+              width={348}
+              height={545}
+              alt={currentProduct.name}
+            />
+          </div>
+        )}
+        {selectedProducts &&
+          Boolean(selectedProducts.length) &&
+          selectedProducts.map((product) => (
+            <div key={product.id} className="absolute" style={{ zIndex: product?.category?.layer }}>
+              <Image src={product.variants[0].image} width={348} height={545} alt={product.name} />
+            </div>
+          ))}
       </div>
-      <SelectPanel categories={categories} />
+      <SelectPanel
+        currentProduct={currentProduct}
+        currentCategory={currentCategory}
+        categories={categories}
+        onCategoryClick={onCategoryClick}
+        onProductClick={onProductClick}
+        onBackButtonClick={onBackButtonClick}
+        onSelectProduct={onSelectProduct}
+      />
     </div>
   );
 }
+
 interface SelectPanelProps {
-  categories: Awaited<ReturnType<typeof getAllCategories>>;
+  categories: Awaited<ReturnType<typeof getCategories>>;
+  currentProduct: ProductType;
+  currentCategory: CategoryType;
+  onCategoryClick: (id: number) => void;
+  onProductClick: (id: number) => void;
+  onBackButtonClick: () => void;
+  onSelectProduct: () => void;
 }
 
-const SelectPanel = ({ categories }: SelectPanelProps) => {
+const SelectPanel = ({
+  currentProduct,
+  currentCategory,
+  categories,
+  onCategoryClick,
+  onProductClick,
+  onBackButtonClick,
+  onSelectProduct,
+}: SelectPanelProps) => {
+  const topCategories = categories.filter((c) => c.parentId === null);
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const topCategories = categories.filter((category) => category.parentId === null);
-  const onCategoryClick = (id: number) => {
-    console.log(id);
-  };
 
   return (
     <div
       className={`absolute w-full h-[21.25rem] transform ${
         isOpen ? "translate-y-0" : "translate-y-[20rem]"
-      } transition-transform bottom-0 left-1/2 -translate-x-1/2 px-2`}
+      } transition-transform bottom-0 left-1/2 -translate-x-1/2 px-2 z-30`}
     >
       <div className=" h-full w-full bg-white rounded-t-[0.625rem] pt-[1.875rem]">
-        <div className={isOpen ? "flex flex-col items-center prose" : "hidden"}>
-          <h2>Customize Lab</h2>
-          <p>Tùy chỉnh ngoại hình- sáng tạo Bakia của bạn</p>
-          <Swiper className="w-full" pagination={{ el: ".swiper-pagination", type: "bullets" }}>
-            <SwiperSlide>
-              <div className="w-full px-[3.25rem] grid grid-cols-3 grid-rows-2 gap-x-[1.875rem] mt-[1.125rem] gap-y-2">
-                {topCategories.slice(0, MAX_COUNT).map((category) => (
-                  <CategoryButton
-                    key={category.id}
-                    category={category}
-                    onCategoryClick={() => onCategoryClick(category.id)}
-                  />
-                ))}
+        <div className={isOpen ? "flex flex-col items-center prose h-full" : "hidden"}>
+          {currentCategory ? (
+            <>
+              <div
+                className="flex items-center justify-between px-2 mx-4 border-b h-9"
+                style={{ width: "calc(100% - 2rem)" }}
+              >
+                <button
+                  className="flex items-center justify-center gap-2 cursor-pointer"
+                  onClick={onBackButtonClick}
+                >
+                  <svg
+                    width="24"
+                    height="16"
+                    viewBox="0 0 24 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M22.6364 6.66071H4.63636L8.59091 2.77679C9.13636 2.24107 9.13636 1.4375 8.59091 0.901786C8.04545 0.366071 7.22727 0.366071 6.68182 0.901786L0.409091 7.0625C-0.136364 7.59821 -0.136364 8.40179 0.409091 8.9375L6.68182 15.0982C7.22727 15.6339 8.04545 15.6339 8.59091 15.0982C9.13636 14.5625 9.13636 13.7589 8.59091 13.2232L4.63636 9.33929H22.6364C23.3182 9.33929 24 8.80357 24 8C24 7.19643 23.3182 6.66071 22.6364 6.66071Z"
+                      fill="black"
+                    />
+                  </svg>
+                  <p>Quay lại</p>
+                </button>
+                <h3>{currentCategory.name}</h3>
               </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="w-full px-[3.25rem] grid grid-cols-3 grid-rows-2 gap-x-[1.875rem] mt-[1.125rem] gap-y-2">
-                {topCategories.slice(MAX_COUNT, topCategories.length).map((category) => (
-                  <CategoryButton
-                    key={category.id}
-                    category={category}
-                    onCategoryClick={() => onCategoryClick(category.id)}
+              <div className="w-full h-full">
+                {currentCategory.children.length ? (
+                  <div className="grid grid-cols-3 grid-row-2 gap-x-[1.875rem] mt-[1.125rem] gap-y-2 h-full px-[3.25rem]">
+                    {currentCategory.children.map((category) => (
+                      <CategoryButton
+                        key={category.id}
+                        category={category}
+                        onCategoryClick={() => onCategoryClick(category.id)}
+                      />
+                    ))}
+                  </div>
+                ) : (
+                  <SelectProductPanel
+                    products={currentCategory.products}
+                    currentProduct={currentProduct}
+                    onProductClick={onProductClick}
+                    onSelectProduct={onSelectProduct}
                   />
-                ))}
+                )}
               </div>
-            </SwiperSlide>
-          </Swiper>
-          <div className="swiper-pagination" />
+            </>
+          ) : (
+            <div className="flex flex-col items-center w-full">
+              <h2>Customize Lab</h2>
+              <p>Tùy chỉnh ngoại hình- sáng tạo Bakia của bạn</p>
+              <Swiper
+                className="w-full h-full"
+                pagination={{ el: ".swiper-pagination", type: "bullets" }}
+              >
+                <SwiperSlide>
+                  <div className="w-full h-full px-[3.25rem] grid grid-cols-3 grid-rows-2 gap-x-[1.875rem] mt-[1.125rem] gap-y-2 pb-2">
+                    {topCategories.slice(0, MAX_COUNT).map((category) => (
+                      <CategoryButton
+                        key={category.id}
+                        category={category}
+                        onCategoryClick={() => onCategoryClick(category.id)}
+                      />
+                    ))}
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className="w-full px-[3.25rem] grid grid-cols-3 grid-rows-2 gap-x-[1.875rem] mt-[1.125rem] gap-y-2  pb-2">
+                    {topCategories.slice(MAX_COUNT, topCategories.length).map((category) => (
+                      <CategoryButton
+                        key={category.id}
+                        category={category}
+                        onCategoryClick={() => onCategoryClick(category.id)}
+                      />
+                    ))}
+                  </div>
+                </SwiperSlide>
+              </Swiper>
+              <div className="swiper-pagination" />
+            </div>
+          )}
         </div>
       </div>
       <button
@@ -142,18 +267,82 @@ const SelectPanel = ({ categories }: SelectPanelProps) => {
   );
 };
 
+interface SelectProductPanelProps {
+  products: ProductType[];
+  currentProduct: ProductType;
+  onProductClick: (id: number) => void;
+  onSelectProduct: () => void;
+}
+const SelectProductPanel = ({
+  products,
+  currentProduct,
+  onProductClick,
+  onSelectProduct,
+}: SelectProductPanelProps) => {
+  const hasColor = products.some((product) => product.variants.some((variant) => variant.colorId));
+
+  return (
+    <div className="w-full h-full flex flex-col justify-between space-y-[0.75rem] px-2 pb-4">
+      <div className="flex flex-col justify-center min-h-[9.125rem] mb-[1.375rem] mt-[0.875rem] flex-grow gap-3">
+        {hasColor && (
+          <div className="h-[3.125rem] w-full bg-red-500  overflow-x-scroll overflow-y-hidden"></div>
+        )}
+        <div className="flex items-center gap-1 overflow-x-scroll overflow-y-hidden">
+          <div
+            key={0}
+            className={`w-[4.875rem] h-[4.875rem] flex-shrink-0 flex items-center justify-center border rounded-lg ${
+              !currentProduct ? "border border-black border-solid" : "border-none"
+            } bg-background`}
+            onClick={() => onProductClick(0)}
+          />
+          {products.map((product) => (
+            <div
+              key={product.id}
+              className={`w-[4.875rem] h-[4.875rem] flex-shrink-0 flex items-center justify-center border rounded-lg ${
+                currentProduct?.id === product.id
+                  ? "border border-black border-solid"
+                  : "border-none"
+              }`}
+            >
+              <div className="w-16 h-16 cursor-pointer" onClick={() => onProductClick(product.id)}>
+                <Image
+                  src={product.variants[0].thumbnail}
+                  alt={product.name}
+                  width={64}
+                  height={64}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <button
+        className="flex items-center justify-center text-xl italic font-black uppercase rounded-lg h-[3.125rem]
+          text-black bg-main disabled:bg-grey3 disabled:text-opacity-[0.4]"
+        disabled={currentProduct === null}
+        onClick={onSelectProduct}
+      >
+        chọn
+      </button>
+    </div>
+  );
+};
+
 interface CategoryButtonProps {
-  category: ArrayElement<Awaited<ReturnType<typeof getAllCategories>>>;
+  category: Category;
   onCategoryClick: () => void;
 }
+
 const CategoryButton = ({ category, onCategoryClick }: CategoryButtonProps) => {
   return (
     <div
-      className="w-[3.75rem] flex flex-col items-center cursor-pointer"
+      className="flex flex-col items-center w-full cursor-pointer max-h-[5.25rem]"
       onClick={onCategoryClick}
     >
       <Image src={category.thumbnail} alt={category.name} width={60} height={60} />
-      <span className="mt-1">{category.name}</span>
+      <span className="mt-1 text-base leading-5 text-center tracking-[-0.019rem]">
+        {category.name}
+      </span>
     </div>
   );
 };
