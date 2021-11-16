cda517c634edf33b9a335152d1da02a85596f70b
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2021-11-16 12:40:13 +0700

    Cart markup

--- components/CartItemCard.tsx
+++ components/CartItemCard.tsx
@@ -60,7 +60,7 @@ const Card = ({
   }, [isDiscountValid, quantity, onChangeQuantity, selectedVariants]);
 
   return (
-    <div className="flex flex-col mx-4 mt-[0.625rem] md:flex-row md:gap-14 md:max-h-[23.375rem] md:h-full">
+    <div className="flex flex-col mx-4 mt-[0.625rem] md:flex-row md:gap-14 md:max-h-[24.75rem] md:h-full md:border-b md:border-b-altGrey md:pb-8">
       <div className="flex flex-col-reverse mb-6 md:hidden">
         <p className="mobile-body-txt">{item.gene.name}</p>
         <p className="text-black heading-3">{item.gene.description}</p>
@@ -76,7 +76,7 @@ const Card = ({
           </div>
         </div>
         <div className="flex flex-col w-full max-w-[12.75rem] max-h-[12.5rem] relative md:max-w-full md:w-[21.875rem] md:flex-none md:max-h-full">
-          <div className="flex justify-between mb-4">
+          <div className="flex justify-between mb-4 md:mb-2">
             <Link href={`/custom/?id=${item.id}`}>
               <a className="flex items-center gap-1 text-darkMint">
                 <svg
@@ -96,7 +96,7 @@ const Card = ({
             </Link>
             <h3 className="heading-3">chi tiết</h3>
           </div>
-          <dl className="w-full flex flex-col gap-[0.875rem] bg-white px-5 py-3 overflow-y-auto rounded ">
+          <dl className="w-full flex flex-col gap-[0.875rem] bg-white px-5 py-3 overflow-y-auto rounded md:max-h-[12.25rem] md:flex-none md:mb-[1.375rem]">
             <div className="flex flex-col">
               <dt className="text-altGrey">Bakia model</dt>
               <dt>{gene.name}</dt>
@@ -111,16 +111,77 @@ const Card = ({
             ))}
           </dl>
           <div className="absolute bottom-0 w-full h-10 rounded-b faded-bg md:hidden" />
+          <div className="flex-wrap hidden md:flex md:justify-around md:gap-5">
+            <div className="flex justify-between w-full px-6">
+              <div>
+                <div className="text-altGrey">Đơn giá</div>
+                <div className="">{format(price, gene.currency.abbreviationSign)}</div>
+              </div>
+              <div className="md:w-max">
+                <div className="text-altGrey">Số lượng</div>
+                <div className="flex items-center justify-between gap-5">
+                  <button
+                    disabled={disableMinus}
+                    className="text-black disabled:text-altGrey disabled:cursor-not-allowed"
+                    onClick={decreaseQuantity}
+                  >
+                    <svg
+                      width="16"
+                      height="4"
+                      viewBox="0 0 16 4"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M0 0H16V4H0V0Z" fill="currentColor" />
+                    </svg>
+                  </button>
+                  {quantity}
+                  <button
+                    disabled={disablePlus}
+                    onClick={increaseQuantity}
+                    className="text-black disabled:text-altGrey disabled:cursor-not-allowed"
+                  >
+                    <svg
+                      width="18"
+                      height="17"
+                      viewBox="0 0 18 17"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M7.22293 9.98472H0V7.01529H7.22293V0H10.7771V7.01529H18V9.98472H10.7771V17H7.22293V9.98472Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-between w-full">
+              <div className="max-w-[11.625rem] w-full grid place-content-center flex-none">
+                <input
+                  className="w-full h-[2.625rem] pl-5"
+                  placeholder="Nhập code ưu đãi"
+                  onChange={(e) => onChangeDiscountCode(e.target.value)}
+                  value={itemDiscountCode}
+                />
+              </div>
+              <div className="mr-10 min-w-[4rem]">
+                <div className="text-altGrey">Giảm</div>
+                <div className="">{format(discountAmount, gene.currency.abbreviationSign)}</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="flex flex-col justify-between md:gap-5 md:h-full">
+      <div className="flex flex-col justify-between md:gap-5 ">
         <h3 className="hidden text-right md:block heading-3">định giá</h3>
-        <div className="flex flex-wrap gap-[1.625rem] justify-between md:flex-nowrap md:flex-col w-full h-full md:justify-between md:border-t md:border-b md:border-altGrey py-4 md:gap-0">
-          <div className="flex-none">
+        <div className="flex flex-wrap gap-[1.625rem] justify-between md:flex-nowrap md:flex-col w-full h-full md:justify-between md:border-t md:border-b md:border-altGrey py-4 md:gap-0 md:hidden">
+          <div className="flex-none md:hidden">
             <div className="text-altGrey">Đơn giá</div>
             <div className="">{format(price, gene.currency.abbreviationSign)}</div>
           </div>
-          <div className="max-w-[12.75rem] w-full grid place-content-center flex-none">
+          <div className="max-w-[12.75rem] w-full grid place-content-center flex-none  md:hidden">
             <input
               className="w-full h-[2.625rem] pl-5"
               placeholder="Nhập code ưu đãi"
@@ -128,7 +189,7 @@ const Card = ({
               value={itemDiscountCode}
             />
           </div>
-          <div className="md:w-max">
+          <div className="md:w-max md:hidden">
             <div className="text-altGrey">Số lượng</div>
             <div className="flex items-center justify-between gap-5">
               <button
@@ -167,23 +228,23 @@ const Card = ({
               </button>
             </div>
           </div>
-          <div className="max-w-[12.75rem] w-full ">
+          <div className="max-w-[12.75rem] w-full  md:hidden">
             <div className="text-altGrey">Giảm</div>
             <div className="">{format(discountAmount, gene.currency.abbreviationSign)}</div>
           </div>
         </div>
-        <div className="flex items-end justify-between pt-6 pb-4 text-left border-b border-altGrey md:block md:border-none">
-          <div>
-            <div className="heading-3 md:text-mainGray md:body-txt md:capitalize md:not-italic md:font-normal">
+        <div className="flex items-end justify-between pt-6 pb-4 text-left border-b border-altGrey md:flex-col md:border-none md:h-full md:justify-between">
+          <div className="md:text-right">
+            <div className="heading-3 md:text-mainGray md:body-txt md:not-italic md:font-normal md:uppercase">
               Tổng
             </div>
             <div className="font-bold mobile-body-txt text-darkMint md:text-black md:font-normal">
               {format(total, gene.currency.abbreviationSign)}
             </div>
           </div>
-          <div className="flex gap-5">
+          <div className="flex gap-5 md:flex-col-reverse md:gap-3">
             <button
-              className="flex items-center gap-2 text-mainGray"
+              className="flex items-center gap-3 text-mainGray"
               onClick={() => onRemoveCartItem(item.id)}
             >
               <svg
