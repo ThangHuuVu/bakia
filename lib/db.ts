1bca7bd09389572ac4b41f8be790d842b7b5731d
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2021-11-29 23:41:38 +0700

    create order

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,9 @@
 import prisma from "./prisma";
 import { ArrayElement } from "./types/common";
+import { CartItem } from "@/lib/types/cart";
+import { ShippingInfo } from "@/lib/types/payment";
+import { PaymentInfo } from "@/lib/types/payment";
+import { OrderStatus } from "@prisma/client";
 
 export const getCategories = async () => {
   try {
@@ -49,6 +53,7 @@ export const getGene = async () => {
   try {
     const gene = await prisma.gene.findFirst({
       select: {
+        id: true,
         name: true,
         description: true,
         price: true,
@@ -73,5 +78,56 @@ export const getDiscountCode = async () => {
     console.error(e);
     return null;
   }
+};
 
-}
+export const createOrder = async (
+  items: CartItem[],
+  shippingInfo: ShippingInfo,
+  paymentInfo: PaymentInfo
+) => {
+  try {
+    const allOrders = await prisma.order.count();
+    const order = await prisma.order.create({
+      data: {
+        id: `BAKIA-${allOrders + 1}`,
+        paymentMethod: paymentInfo.paymentSource.type,
+        accountHolderName: paymentInfo.paymentSource.accountName,
+        accountNumber: paymentInfo.paymentSource.accountNumber,
+        status: OrderStatus.PENDING,
+        total: items.reduce((res, current) => {
+          return res + current.total || 0;
+        }, 0),
+        user: {
+          create: {
+            fullName: shippingInfo.fullName,
+            phoneNumber: shippingInfo.phoneNumber,
+            email: shippingInfo.email,
+            address: shippingInfo.address,
+            gender: shippingInfo.gender,
+            area: shippingInfo.area,
+            note: shippingInfo.note,
+            shippingMethod: shippingInfo.shippingMethod,
+          },
+        },
+        items: {
+          create: items.map((item) => ({
+            total: item.total,
+            discountCode: {
+              connect: { id: item.discountCodeId },
+            },
+            gene: {
+              connect: { id: item.gene.id },
+            },
+            productVariants: {
+              connect: item.selectedVariants.map((variant) => ({ id: variant.id })),
+            },
+          })),
+        },
+      },
+    });
+    return order;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
--- lib/types/payment.ts
+++ lib/types/payment.ts
@@ -1,17 +1,9 @@
-enum GenderEnum {
-  female = "female",
-  male = "male",
-}
+import { Gender, PaymentMethod, ShippingMethod } from ".prisma/client";
 
 enum AreaEnum {
   south = "south",
   north = "north",
   mid = "mid",
-}
-
-export enum ShippingMethodEnum {
-  standard = "standard",
-  fast = "fast",
 }
 
 export const ShippingDetail = {
@@ -40,19 +32,19 @@ export const PaymentMethodDetail = {
 
 export type ShippingInfo = {
   fullName: string;
-  gender: GenderEnum;
+  gender: Gender;
   email: string;
   phoneNumber: string;
   area: AreaEnum;
   address: string;
-  shippingMethod: ShippingMethodEnum;
+  shippingMethod: ShippingMethod;
   note: string;
 };
 
 type PaymentSource = {
   accountNumber: string;
   accountName: string;
-  type: PaymentMethodEnum;
+  type: PaymentMethod;
 };
 
 export type PaymentInfo = {
@@ -60,7 +52,7 @@ export type PaymentInfo = {
   paymentSource: PaymentSource;
 };
 
-interface PaymentMethod {
+interface Payment {
   accountHolderName: string;
   accountNumber: string;
   name: string;
@@ -70,11 +62,11 @@ interface PaymentMethod {
   };
 }
 
-export interface BankAccount extends PaymentMethod {
+export interface BankAccount extends Payment {
   branch: string;
 }
 
-export interface EWallet extends PaymentMethod {}
+export interface EWallet extends Payment {}
 
 export type PaymentContent = {
   term: any;
