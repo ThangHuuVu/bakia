b49efcf77bb8e091967ccabdc12a2f41324dbd13
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2021-12-03 17:57:45 +0700

    prisma migration: add quantity in order item

---
+++ prisma/migrations/20211203105635_add_order_item_quantity/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - Added the required column `quantity` to the `OrderItem` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `OrderItem` ADD COLUMN `quantity` INTEGER NOT NULL;
